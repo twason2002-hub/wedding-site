7086821ef8ef6d736076540b48a86b72cfdec6f9
tusharwason <tushar.wasson@flixbus.com>
2025-12-29 13:58:16 +0100

    Adding changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,9 @@ export const metadata: Metadata = {
   title: "Tushar & Babitta | Wedding Invitation",
   description: "You're cordially invited to join us on a day of love and celebration as we tie the knot on 15th Feb! 🥳💕 It would mean the world to us if you could be there to share in the joy, laughter, and happily-ever-after vibes 😊. Looking forward to seeing you there! 🎉",
   icons: {
-    icon: '/images/back-5.jpg',
+    icon: '/favicon.ico',
+    shortcut: '/favicon.ico',
+    apple: '/images/back-5.jpg',
   },
   openGraph: {
     title: "Tushar & Babitta | Wedding Invitation",
